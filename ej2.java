a01f58a46e41f90251e8dd91c3a05f7b93d43e19
teamMOYA <32859499+teamMOYA@users.noreply.github.com>
2018-04-13 12:57:54 +0200

    Create ej2.java

---
+++ ej2.java
@@ -0,0 +1,65 @@
+
+import java.io.FileInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.security.Key;
+import java.security.MessageDigest;
+import java.util.Arrays;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+public class Cifrado {
+  	
+  
+	//FUNCION QUE PIDE FICHERO Y UNA PASS(STRING)
+	public static byte[] cifrarFichero(String file, String password) throws FileNotFoundException{
+      
+      	byte[] data= null;
+      	//creamos fichero
+		File filec = new File("./src",file);
+      	//comprobamos si existe
+      	if (filec.exists()){
+			//generamos clave
+          	keygen(password);
+          	//obtenemos datos fichero
+          	getBytesFromFile(filec);
+          	//ciframos fichero
+          	
+        }else{
+          throw new FileNotFoundException();
+        }
+      	return data;
+    }
+  
+  	private static SecretKey keygen(String password){
+        SecretKey sKey = null;
+        int key_sice = 192;
+        try {
+			byte[] data = password.getBytes("UTF-8");
+             MessageDigest md = MessageDigest.getInstance("SHA-256");
+             byte[] hash = md.digest(data);
+             byte[] key = Arrays.copyOf(hash, key_sice/8);
+             sKey = new SecretKeySpec(key, "AES");
+		} catch (Exception ex) {
+             System.err.println("Error generant la clau:" + ex);
+		}
+        return sKey;
+
+	}
+  	
+  	private static Byte[] getBytesFromFile(File file){
+    	byte[] bytesArray = new byte[(int) file.length()];
+     
+      	FileInputStream fis = new FileInputStream(file);
+  		fis.read(bytesArray); //read file into bytes[]
+  		fis.close();
+			
+  		return bytesArray;
+    }
+      
+    
+
+
+}
